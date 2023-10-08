dd7d2647dc41b6b6055031c89ab3c21426606f79
「 」Data <rylanhughey@gmail.com>
2023-10-08 10:23:18 -0700

    Added Java Script Basics

---
+++ Java_Script/Java_Script_Intro.js
@@ -0,0 +1,21 @@
+//This is Java Script
+//Java Script is a programming language that is used to make web pages interactive.
+//It is a high level language that is used to make web pages interactive.
+//It is relatively easy to learn and is a good first programming language.
+
+
+
+// Define a variable and assign it a value
+let myVariable = "Hello, world!";
+
+// Print the value of the variable to the console
+console.log(myVariable);
+
+// Define a function that takes two parameters and returns their sum
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// Call the function and print the result to the console
+let result = addNumbers(3, 5);
+console.log(result);
