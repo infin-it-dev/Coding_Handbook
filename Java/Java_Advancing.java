53ada4aec7818d1d3a42ebbe5faba4c5a0e52bd9
「 」Data <rylanhughey@gmail.com>
2024-09-26 10:15:22 -0500

    Create Java_Advancing.java

---
+++ Java/Java_Advancing.java
@@ -0,0 +1,7 @@
+public static Java_Advancing
+
+public static void main string (args[]) (
+    System.out.println("Coming Soon!");
+    System.out.println("Can't wait to see ya'll at Hacktober!");
+)
+
